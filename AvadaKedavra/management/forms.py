b0e7bee8e441e8f23c82149d6722b3fdb91ade83
Yorchly <jorge.gutierrezvila0@gmail.com>
2018-12-28 16:01:50 +0100

    Creado requirements.txt, plantilla prototipo para el form de user y cambios variados en algunos ficheros

---
+++ AvadaKedavra/management/forms.py
@@ -0,0 +1,22 @@
+from datetimewidget.widgets import DateWidget
+from django import forms
+from models import User
+
+class UserCreateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        exclude = ['project']
+        dateTimeOptions = {
+            'format': 'dd/mm/yyyy',
+            'autoclose': True,
+            'weekStart': 1,
+            'todayHighlight': True,
+            'language': 'es',
+        }
+        widgets = {
+            'birthdate': DateWidget(attrs={'id': "birthdate"}, options=dateTimeOptions, bootstrap_version=3),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+
