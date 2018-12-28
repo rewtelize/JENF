047f4bca891f0363e62ffa5859883f335693ee95
Javier <javibato.8@gmail.com>
2018-12-28 18:36:19 +0100

    Users can be showed

--- AvadaKedavra/management/urls.py
+++ AvadaKedavra/management/urls.py
@@ -6,6 +6,8 @@ from views import UserCreateView
 
 urlpatterns = [
     url(r'^management/', views.management, name='management'),
-    url(r'^$', views.management, name='management'),
     url(r'^crearUsuario', UserCreateView.as_view(), name="create-user"),
+    url(r'^deleteUser/(?P<pk>[0-9a-f-]+)/$', views.delete_user, name='delete_user'),
+    url(r'^users', views.show_users, name='users'),
+    url(r'^$', views.management, name='management'),
 ]
--- AvadaKedavra/management/views.py
+++ AvadaKedavra/management/views.py
@@ -2,15 +2,24 @@
 from __future__ import unicode_literals
 
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView, FormView, View, DetailView
-from django.shortcuts import render
+
 from forms import UserCreateForm
+from models import User
 
 
 def management(request):
 	return render(request, 'management.html')
 
+def show_users(request):
+	users = User.objects.order_by('country').order_by('firstname').order_by('lastname')
+	return render(request, 'users.html', {'users':users})
+
+def delete_user(request, pk):
+    return HttpResponse("Delete user")
 
 class UserCreateView(SuccessMessageMixin, CreateView, FormView):
 	template_name = "userCreate.html"
@@ -22,4 +31,5 @@ class UserCreateView(SuccessMessageMixin, CreateView, FormView):
 		self.object = form.save(commit=False)
 		self.object.save()
 		return super(UserCreateView, self).form_valid(form)
+
 
