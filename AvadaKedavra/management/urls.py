94bf976e39229f5f177eac25b512653ca400cc48
Javier <javibato.8@gmail.com>
2018-12-29 19:21:43 +0100

    show and delete Organizations functions added

--- AvadaKedavra/management/urls.py
+++ AvadaKedavra/management/urls.py
@@ -5,9 +5,11 @@ from django.conf import settings
 from views import UserCreateView
 
 urlpatterns = [
-    url(r'^management/', views.management, name='management'),
     url(r'^crearUsuario', UserCreateView.as_view(), name="create-user"),
     url(r'^deleteUser/(?P<pk>[0-9a-f-]+)/$', views.delete_user, name='delete_user'),
+    url(r'^deleteOrganization/(?P<pk>[0-9a-f-]+)/$', views.delete_organization, name='delete_organization'),
+    url(r'^management/', views.management, name='management'),
+    url(r'^organizations', views.show_organizations, name='organizations'),
     url(r'^users', views.show_users, name='users'),
     url(r'^$', views.management, name='management'),
 ]
--- AvadaKedavra/management/views.py
+++ AvadaKedavra/management/views.py
@@ -3,23 +3,32 @@ from __future__ import unicode_literals
 
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView, FormView, View, DetailView
 
 from forms import UserCreateForm
-from models import User
+from models import Organization, User
 
 
 def management(request):
 	return render(request, 'management.html')
+
+def show_organizations(request):
+	organizations = Organization.objects.order_by('country').order_by('name')
+	return render(request, 'organizations.html', {'organizations':organizations})
 
 def show_users(request):
 	users = User.objects.order_by('country').order_by('firstname').order_by('lastname')
 	return render(request, 'users.html', {'users':users})
 
 def delete_user(request, pk):
-    return HttpResponse("Delete user")
+	User.objects.filter(id=pk).delete()
+	return redirect('users')
+
+def delete_organization(request, pk):
+	Organization.objects.filter(id=pk).delete()
+	return redirect('organizations')
 
 class UserCreateView(SuccessMessageMixin, CreateView, FormView):
 	template_name = "userCreate.html"
