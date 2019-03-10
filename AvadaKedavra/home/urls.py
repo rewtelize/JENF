6ce692a5569d91dfed4f3bdd2c3f14fd42e39aab
Javier <javibato.8@gmail.com>
2019-03-10 17:01:01 +0100

    Template for projects included

--- AvadaKedavra/home/urls.py
+++ AvadaKedavra/home/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^home', views.home, name='home'),
+    url(r'^exchange', views.exchange, name='exchange'),
     url(r'^$', views.home, name='home'),
 ]
--- AvadaKedavra/home/views.py
+++ AvadaKedavra/home/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request, 'home.html')
+
+def exchange(request):
+	return render(request, 'exchange.html')
